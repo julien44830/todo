bd66111cc5c8ac09fdb4368dc242dd81f1eea139
julien44830 <deniaudjuli0@gmail.com>
2024-07-19 09:28:52 +0200

    pour prettier

--- server/shema.sql
+++ server/shema.sql
@@ -7,17 +7,17 @@ CREATE TABLE tache (
     description VARCHAR(50) NOT NULL
 );
 
+
+-- cette table n'est pas utilisé
 CREATE TABLE soustache (
     id_soustache INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     tache_id INT NOT NULL,
     description VARCHAR(50) NOT NULL,
-    FOREIGN KEY (tache_id) REFERENCES tache(id)
+    FOREIGN KEY (tache_id) REFERENCES tache(id) ON DELETE CASCADE
 );
-
 
 INSERT INTO tache (titre, description) VALUES ('Tâche 1', 'Description de la tâche 1');
 INSERT INTO tache (titre, description) VALUES ('Tâche 2', 'Description de la tâche 2');
-
 
 INSERT INTO soustache (tache_id, description) VALUES (1, 'Description de la sous-tâche 1 pour la tâche 1');
 INSERT INTO soustache (tache_id, description) VALUES (1, 'Description de la sous-tâche 2 pour la tâche 1');
