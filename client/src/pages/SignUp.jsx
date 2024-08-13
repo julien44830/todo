04448a27a74ba49394dafa11fca7c19c291c8ce4
julien44830 <deniaudjulien0@gmail.com>
2024-08-13 04:13:26 +0200

    verify password front ok

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,185 +3,203 @@ import { NavLink } from "react-router-dom";
 import Header from "../components/Header";
 
 export default function SingUp() {
-  const [formData, setFormData] = useState({
-    email: "",
-    name: "",
-    password: "",
-    checkPassword: "",
-  });
-
-  const [focusedField, setFocusedField] = useState(null);
-  const [checkedPassword, setCheckedPassword] = useState({
-    password: true,
-    verifPassword: false,
-  });
-  console.log("%c⧭", "color: #e50000", checkedPassword);
-
-  // Fonction appelée lorsque l'élément reçoit le focus
-  const handleFocus = (fieldName) => {
-    setFocusedField(fieldName);
-  };
-
-  // Fonction appelée lorsque l'élément perd le focus
-  const handleBlur = () => {
-    setFocusedField(null);
-  };
-
-  // Fonction pour gérer les changements dans les champs
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+    const [formData, setFormData] = useState({
+        email: "",
+        name: "",
+        password: "",
+        checkPassword: "",
     });
-  };
 
-  const getInputClassName = (fieldName) => {
-    return `p-input ${
-      focusedField === fieldName || formData[fieldName] ? "p-input-focus" : ""
-    }`;
-  };
-
-  console.log("%c⧭", "color: #aa00ff", formData);
-
-  const reg = "rerr";
-  const isValidPassword = formData.password.includes(reg);
-
-  const forcePassword = () => {
-    if (!formData.password.includes(reg)) {
-      setCheckedPassword((prev) => ({
-        ...prev,
-        password: isValidPassword,
-      }));
-    } else {
-      setCheckedPassword((prev) => ({
-        ...prev,
-        password: isValidPassword,
-      }));
-    }
-  };
-
-  const verifPassword = () => {
-    if (formData.password !== formData.checkPassword) {
-      alert("Les mots de passe ne correspondent pas");
-      setCheckedPassword((prev) => ({
-        ...prev,
+    const [focusedField, setFocusedField] = useState(null);
+    const [checkedPassword, setCheckedPassword] = useState({
+        password: true,
         verifPassword: false,
-      }));
-    } else {
-      setCheckedPassword((prev) => ({
-        ...prev,
-        verifPassword: true,
-      }));
-    }
-  };
+    });
+    console.log("%c⧭", "color: #e50000", checkedPassword);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (checkedPassword.password && checkedPassword.verifPassword) {
-      const { name, email, password } = formData;
+    // Fonction appelée lorsque l'élément reçoit le focus
+    const handleFocus = (fieldName) => {
+        setFocusedField(fieldName);
+    };
 
-      try {
-        const response = await fetch("http://localhost:3000/api/user/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name,
-            email,
-            password,
-          }),
+    // Fonction appelée lorsque l'élément perd le focus
+    const handleBlur = () => {
+        setFocusedField(null);
+    };
+
+    // Fonction pour gérer les changements dans les champs
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
         });
+    };
 
-        if (!response.ok) {
-          throw new Error("Erreur lors de l'ajout du User");
+    const getInputClassName = (fieldName) => {
+        return `p-input ${
+            focusedField === fieldName || formData[fieldName]
+                ? "p-input-focus"
+                : ""
+        }`;
+    };
+
+    console.log("%c⧭", "color: #aa00ff", formData);
+
+    const reg =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;"'<>,.?/~`]).{8,}$/;
+
+    const isValidPassword = reg.test(formData.password);
+    const forcePassword = () => {
+        if (!reg.test(formData.password)) {
+            setCheckedPassword((prev) => ({
+                ...prev,
+                password: isValidPassword,
+            }));
+        } else {
+            setCheckedPassword((prev) => ({
+                ...prev,
+                password: isValidPassword,
+            }));
         }
+    };
 
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error("Erreur lors de l'ajout du User :", error);
-      }
-    }
-  };
+    const verifPassword = () => {
+        if (formData.password !== formData.checkPassword) {
+            alert("Les mots de passe ne correspondent pas");
+            setCheckedPassword((prev) => ({
+                ...prev,
+                verifPassword: false,
+            }));
+        } else {
+            setCheckedPassword((prev) => ({
+                ...prev,
+                verifPassword: true,
+            }));
+        }
+    };
 
-  return (
-    <>
-      <Header />
-      <form onSubmit={handleSubmit} className="form-login" method="POST">
-        <p>Créer un compte</p>
-        <label>
-          <p className={getInputClassName("name")}>Nom</p>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onFocus={() => handleFocus("name")}
-            onBlur={handleBlur}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          <p className={getInputClassName("email")}>Email</p>
-          <input
-            type="text"
-            name="email"
-            value={formData.email}
-            onFocus={() => handleFocus("email")}
-            onBlur={handleBlur}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          <p className={getInputClassName("password")}>Mot de passe</p>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onFocus={() => handleFocus("password")}
-            onBlur={() => {
-              handleBlur();
-              forcePassword();
-            }}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          <p className={getInputClassName("checkPassword")}>
-            Confirmation de mot de passe
-          </p>
-          <input
-            type="password"
-            name="checkPassword"
-            value={formData.checkPassword}
-            onFocus={() => handleFocus("checkPassword")}
-            onBlur={() => {
-              handleBlur();
-              verifPassword();
-            }}
-            onChange={handleChange}
-            required
-          />
-        </label>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (checkedPassword.password && checkedPassword.verifPassword) {
+            const { name, email, password } = formData;
 
-        <button type="submit" className="btn-login">
-          Créer ton compte
-        </button>
-        <NavLink to="/connexion">
-          <p>Déjà un compte ? Connecte toi</p>
-        </NavLink>
-        {checkedPassword.password ? null : (
-          <p>
-            Ton mot de passe doit contenir 1 Majuscule, 1 minuscule, 1 chiffre
-            et 1 carractère spéciale.
-          </p>
-        )}
-      </form>
-      <img className="img-home" src="stylo_plume.png" alt="stylo plume" />
-    </>
-  );
+            try {
+                const response = await fetch(
+                    "http://localhost:3000/api/user/",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            name,
+                            email,
+                            password,
+                        }),
+                    }
+                );
+
+                if (!response.ok) {
+                    throw new Error("Erreur lors de l'ajout du User");
+                }
+
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error("Erreur lors de l'ajout du User :", error);
+            }
+        }
+    };
+
+    return (
+        <>
+            <Header />
+            <form
+                onSubmit={handleSubmit}
+                className="form-login"
+                method="POST"
+            >
+                <p>Créer un compte</p>
+                <label>
+                    <p className={getInputClassName("name")}>Nom</p>
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onFocus={() => handleFocus("name")}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <label>
+                    <p className={getInputClassName("email")}>Email</p>
+                    <input
+                        type="text"
+                        name="email"
+                        value={formData.email}
+                        onFocus={() => handleFocus("email")}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <label>
+                    <p className={getInputClassName("password")}>
+                        Mot de passe
+                    </p>
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onFocus={() => handleFocus("password")}
+                        onBlur={() => {
+                            handleBlur();
+                            forcePassword();
+                        }}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+                <label>
+                    <p className={getInputClassName("checkPassword")}>
+                        Confirmation de mot de passe
+                    </p>
+                    <input
+                        type="password"
+                        name="checkPassword"
+                        value={formData.checkPassword}
+                        onFocus={() => handleFocus("checkPassword")}
+                        onBlur={() => {
+                            handleBlur();
+                            verifPassword();
+                        }}
+                        onChange={handleChange}
+                        required
+                    />
+                </label>
+
+                <button
+                    type="submit"
+                    className="btn-login"
+                >
+                    Créer ton compte
+                </button>
+                <NavLink to="/connexion">
+                    <p>Déjà un compte ? Connecte toi</p>
+                </NavLink>
+                {checkedPassword.password ? null : (
+                    <p>
+                        Ton mot de passe doit contenir 1 Majuscule, 1 minuscule,
+                        1 chiffre et 1 carractère spéciale.
+                    </p>
+                )}
+            </form>
+            <img
+                className="img-home"
+                src="stylo_plume.png"
+                alt="stylo plume"
+            />
+        </>
+    );
 }
